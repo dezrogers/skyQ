6c5ad0af923af78f5e144ebbe4333f4f8f0a51eb
dezroge <desmondrogers2@gmail.com>
2019-03-19 14:07:14 -0500

    resolved conflicts

--- logic.js
+++ logic.js
@@ -1,68 +1,9 @@
-// api call - predict the sky
-
-// api call - geolocation
-
-// on submit -- convery city to lat/lon and pull relevant data from predict the sky
-
-// rss with up to date nasa news?
-
-// var with date to pass into api parameters
-$('#submitButton').on('click', function() {
-  var date = $('#date').val().toString();
-  var date2 = moment(date).format('MM/DD/YYYY'); 
-  console.log(date);
-  console.log(date2);
-  nearEarth(date);
-  moonPhase(date2);
-
-// this totally works w00t
-var queryURL = "http://api.open-notify.org/iss-now.json";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-
-// moonphase api call --- populate into table?
-function moonPhase() {
-var moonPhaseURL = "https://api.usno.navy.mil/moon/phase?date=" + date2 + "&nump=4";
-
-$.ajax({
-  url: moonPhaseURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-}
-
-// Near Earth Objects NASA api key: w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT
-function nearEarth() {
-var neoWSURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + date + "&api_key=w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT";
-
-$.ajax({
-  url: neoWSURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-}
-
-// on click closing tag. dont fuck with this
-})
-$("#submitButton").on("click", function(e) {
-    e.preventDefault();
-    window.location.href = "main.html";
-
-});
-$("#returnButton").on("click", function(e) {
-    e.preventDefault();
-    window.location.href = "index.html";
-
-});
 
 $(document).ready(function(){
+
+  // api call - geolocation
+
+  // rss with up to date nasa news?
 
     // VARIABLES
     // ---------------------------------
@@ -127,48 +68,99 @@ $(document).ready(function(){
 
 
     // EVENTS
-    // ---------------------------------
-    $("button").click(function(event){
-        event.preventDefault();
+    // ---------------------------------------------------------
+    // var with date to pass into api parameters
+    $('#submitButton').on('click', function() {
+          e.preventDefault();
+          window.location.href = "main.html";
 
-        // if user deny location lat and lon undefined queryURL is input val
-        if ((lat || lon) !== undefined) {
-            weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+weatherAPIKey;
-        } else {
-            var userLocation = $("#location").val().toString();
-            console.log(userLocation);
-            weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip="+userLocation+"&appid="+weatherAPIKey;
-        }
+          var date = $('#date').val().toString();
+          var date2 = moment(date).format('MM/DD/YYYY');
+          console.log(date);
+          console.log(date2);
+          nearEarth(date);
+          moonPhase(date2);
 
+          // this totally works w00t
+          var queryURL = "http://api.open-notify.org/iss-now.json";
 
-        // AJAX call
-        $.ajax ({
-            url: weatherQueryURL,
+          $.ajax({
+            url: queryURL,
             method: "GET"
-        }).then(function(response) {
-
-            // Log the queryURL
-            console.log(weatherQueryURL);
-
-            // log the resulting object
+          }).then(function(response) {
             console.log(response);
+          });
 
-            // transfer content to HTML
-            var weatherDiv = $("#weatherContainer");
+          // moonphase api call --- populate into table?
+          function moonPhase() {
+            var moonPhaseURL = "https://api.usno.navy.mil/moon/phase?date=" + date2 + "&nump=4";
 
-            weatherDiv.append($("<p>").text("City: "+ response.name));
-            $(".wind").html("Wind: "+ response.wind);
-            $(".humidity").html("humidity: "+response.main.humidity);
-            $(".temp").html("Temp: "+response.main.temp);
+            $.ajax({
+              url: moonPhaseURL,
+              method: "GET"
+            }).then(function(response) {
+              console.log(response);
+            });
+          }
 
-            // Calculate the temperature (converted from Kelvin)
-            // To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-            var Fahrenheit = Math.floor(((response.main.temp - 273.15)*1.80 +32));
-            console.log(Fahrenheit + "°F")
+          // Near Earth Objects NASA api key: w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT
+          function nearEarth() {
+            var neoWSURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + date + "&api_key=w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT";
 
-            // Dump the temperature content into HTML
-            $(".temp").html("Temp: "+ Fahrenheit);
+            $.ajax({
+              url: neoWSURL,
+              method: "GET"
+            }).then(function(response) {
+              console.log(response);
+            });
+          }
+
+          // if user deny location lat and lon undefined queryURL is input val
+          if ((lat || lon) !== undefined) {
+              weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+weatherAPIKey;
+          } else {
+              var userLocation = $("#location").val().toString();
+              console.log(userLocation);
+              weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip="+userLocation+"&appid="+weatherAPIKey;
+          }
+
+
+          // AJAX call
+          $.ajax ({
+              url: weatherQueryURL,
+              method: "GET"
+          }).then(function(response) {
+
+              // Log the queryURL
+              console.log(weatherQueryURL);
+
+              // log the resulting object
+              console.log(response);
+
+              // transfer content to HTML
+              var weatherDiv = $("#weatherContainer");
+
+              weatherDiv.append($("<p>").text("City: "+ response.name));
+              $(".wind").html("Wind: "+ response.wind);
+              $(".humidity").html("humidity: "+response.main.humidity);
+              $(".temp").html("Temp: "+response.main.temp);
+
+              // Calculate the temperature (converted from Kelvin)
+              // To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+              var Fahrenheit = Math.floor(((response.main.temp - 273.15)*1.80 +32));
+              console.log(Fahrenheit + "°F")
+
+              // Dump the temperature content into HTML
+              $(".temp").html("Temp: "+ Fahrenheit);
+
+          // on click closing tag. dont fuck with this
+        })
+
+        $("#returnButton").on("click", function(e) {
+          e.preventDefault();
+          window.location.href = "index.html";
 
         });
+
     });
 })
