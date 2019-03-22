fd6da623c9fd830f9f2dfbcd901c00c406598394
dezroge <desmondrogers2@gmail.com>
2019-03-22 13:39:19 -0500

    complete feature

--- logic.js
+++ logic.js
@@ -34,23 +34,16 @@ console.log(28);
     var crd = pos.coords;
     console.log(pos);
 
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
     lat = crd.latitude;
     lon = crd.longitude;
     acc = crd.accuracy;
-    console.log(45);
   }
 
   function error(err) {
       console.warn(`ERROR(${err.code}): ${err.message}`);
   }
 
-  // run geolocation code. success, failure, and the last argument failure.
-  navigator.geolocation.getCurrentPosition(success, error, options);
-console.log(54);
+
   // display information on front page "default value"
   function display() {
     mapsQueryURL = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat="+lat+"&lon="+lon+"";
@@ -59,7 +52,6 @@ console.log(54);
       url: mapsQueryURL,
       method: "GET"
     }).then(function(mapsResponse) {
-      console.log(mapsResponse);
       postCode = mapsResponse.address.postcode;
       console.log(postCode);
       $("#zipCode").attr("value", postCode);
@@ -67,24 +59,100 @@ console.log(54);
     })
   }
   
-  setTimeout(display, 10000);
+
+  function virtualSky(){
+    var vSky = $("<iframe width='900' height='385' frameborder='0' scrolling='no' marginheight='' marginwidth='' src='https://virtualsky.lco.global/embed/index.html?longitude="+lon+"&latitude="+lat+"&projection=gnomic&constellations=true&constellationlabels=true&showplanetlabels=true&meridian=true' allowTransparency='true'></iframe>")
+
+    $("#moonPhases").html(vSky);
+  }
   
+  // moonphase api call --- populate into table?
+  // function moonPhase() {
+  //   var date = $('#date').val().toString();
+  //   var date2 = moment(date).format('MM/DD/YYYY');
+    
+  //   var moonPhaseURL = "https://api.usno.navy.mil/moon/phase?date=" + date2 + "&nump=4";
+
+  //   $.ajax({
+  //     url: moonPhaseURL,
+  //     method: "GET"
+  //   }).then(function(response) {
+  //     $('#moonPhases').empty();
+  //     // populate into #eventsDiv. phasedata [4]
+  //     var phase1 = $('<p>').text('Full Moon: ' + response.phasedata[0].date);
+  //     var phase2 = $('<p>').text('Last Quarter: ' + response.phasedata[1].date);
+  //     var phase3 = $('<p>').text('New Moon: ' + response.phasedata[2].date);
+  //     var phase4 = $('<p>').text('First Quarter: ' + response.phasedata[3].date);
+  //     $("#moonPhases").append(phase1, phase2, phase3, phase4);
+  //   });
+  // }
+
+  /*
+  // Near Earth Objects NASA api key: w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT
+  function nearEarth() {
+    var neoWSURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + date + "&api_key=w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT";
+
+    $.ajax({
+      url: neoWSURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      $("#nearEarth").empty();
+      var nearEarthObjects = $("<p>");
+      nearEarthObjects.text(response.element_count + " current objects near the Earth")
+      $("#nearEarth").append(nearEarthObjects);
+    });
+  }
+  */
+
+  function getWeather(){
+    // if user denies location queryURL is user input value
+    var userLocation = $("#zipCode").val().toString();
+    console.log(userLocation);
+    
+    weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip="+userLocation+"&units=imperial&appid="+weatherAPIKey;
   
+
+    // Weather API - current temp
+    $.ajax ({
+      url: weatherQueryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      // log the resulting object
+      var pCity = $("<h1>").text(response.name+", "+response.sys.country);
+      var pWeather = $("<h4>").text(response.weather[0].main);
+      var pTemp = $("<p>").text("low "+ Math.floor(response.main.temp_min) +"° | "+ "high "+ Math.floor(response.main.temp_max) +"°");
+      var pClouds = $("<td>").text(response.clouds.all +"%");
+      var pHumid = $("<td>").text(response.main.humidity+" %");
+      var pWindSpeed = $("<td>").text(Math.floor(response.wind.speed) + " mph");
+      var pWindDeg = $("<td>").text(response.wind.deg +"°");
+      
+      var iconCode = response.weather[0].icon;
+      var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      var wIcon = $("<img>").attr("id", "weather-icon").attr("alt", "Weather Icon").attr("src", iconUrl);
+      
+      
+      $("#city-name").append(pCity);
+      $("#weather-forecast").append(wIcon, pWeather, pTemp);
+      $("tbody>tr").append(pClouds, pHumid, pWindSpeed, pWindDeg);
+  
+    })
+  } 
+
   // EVENTS
   // ---------------------------------------------------------
+  // run geolocation code. success, failure, and the last argument failure.
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
   var today = moment().format("YYYY-MM-DD");
   $("#date").attr("value", today);
   
+  setTimeout(display, 10000);
+
   $(".hidden").hide();
   
   $("#zipCode").attr("placeholder", "Determining Location...");
-
-  // if (postCode === undefined) {
-  //   $("#zipCode").attr("placeholder", "Zip Code");
-  // } else {
-  //   $("#zipCode").attr("placeholder", "Determining Location...");
-  // }
-
 
   firebase.initializeApp(config);
 
@@ -96,7 +164,6 @@ console.log(54);
     e.preventDefault();
     $(".hidden").show();
 
-
     // Add to clickCounter
     clickCounter++;
 
@@ -105,10 +172,7 @@ console.log(54);
       clickCount: clickCounter
     });
 
-    var date = $('#date').val().toString();
-    var date2 = moment(date).format('MM/DD/YYYY');
-    console.log(date);
-    console.log(date2);
+
 
     /* // called down in webGLearth function
     var queryISSURL = "http://api.open-notify.org/iss-now.json";
@@ -128,92 +192,10 @@ console.log(54);
       console.log("the code for the iss coordinates ran once");
     }); */
 
-    // moonphase api call --- populate into table?
-    function moonPhase() {
-      var moonPhaseURL = "https://api.usno.navy.mil/moon/phase?date=" + date2 + "&nump=4";
 
-      $.ajax({
-        url: moonPhaseURL,
-        method: "GET"
-      }).then(function(response) {
-        $('#moonPhases').empty();
-        console.log(response); 
-        // populate into #eventsDiv. phasedata [4]
-        var phase1 = $('<p>').text('Full Moon: ' + response.phasedata[0].date);
-        var phase2 = $('<p>').text('Last Quarter: ' + response.phasedata[1].date);
-        var phase3 = $('<p>').text('New Moon: ' + response.phasedata[2].date);
-        var phase4 = $('<p>').text('First Quarter: ' + response.phasedata[3].date);
-        /* console.log(phase1);
-        console.log(phase2);
-        console.log(phase3);
-        console.log(phase4); */
-        $("#moonPhases").append(phase1, phase2, phase3, phase4);
-      });
-    }
+    getWeather();
 
-    moonPhase(date2);
-
-    // Near Earth Objects NASA api key: w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT
-    function nearEarth() {
-      var neoWSURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + date + "&api_key=w6WzGfIJHpH3CYm2kyvIAuej0NwIjBmbh1ywubzT";
-
-      $.ajax({
-        url: neoWSURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-        $("#nearEarth").empty();
-        var nearEarthObjects = $("<p>");
-        nearEarthObjects.text(response.element_count + " current objects near the Earth")
-        $("#nearEarth").append(nearEarthObjects);
-      });
-    }
-
-    nearEarth(date);
-
-    // if user denies location queryURL is user input value
-    var userLocation = $("#zipCode").val().toString();
-    console.log(userLocation);
-    weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip="+userLocation+"&units=imperial&appid="+weatherAPIKey;
-    
-    // $("#weatherDiv").empty();
-    
-    // Weather API - current temp
-    $.ajax ({
-      url: weatherQueryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      // Log the queryURL
-      console.log(weatherQueryURL);
-      // CODE for emptying weather data on click
-
-      // log the resulting object
-      console.log(response);
-      var pCity = $("<h1>").text(response.name+", "+response.sys.country);
-      var pWeather = $("<h4>").text(response.weather[0].main);
-      var pTemp = $("<p>").text("low "+ Math.floor(response.main.temp_min) +"° | "+ "high "+ Math.floor(response.main.temp_max) +"°");
-      var pClouds = $("<td>").text(response.clouds.all +"%");
-      var pHumid = $("<td>").text(response.main.humidity+" %");
-      var pWindSpeed = $("<td>").text(Math.floor(response.wind.speed) + " mph");
-      var pWindDeg = $("<td>").text(response.wind.deg +"°");
-      
-      var iconCode = response.weather[0].icon;
-      var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-      var wIcon = $("<img>").attr("id", "weather-icon").attr("alt", "Weather Icon").attr("src", iconUrl);
-      
-      
-      // transfer content to HTML
-      // var weatherCol1 = $("<div>").addClass("col-lg-6");
-      // var weatherCol2 = $("<div>").addClass("col-lg-6");
-      
-      $("#city-name").append(pCity);
-      $("#weather-forecast").append(wIcon, pWeather, pTemp);
-      $("tbody>tr").append(pClouds, pHumid, pWindSpeed, pWindDeg);
-  
-
-
-    })
+    virtualSky();
 
   })
 
@@ -243,31 +225,31 @@ function initialize() {
   }).addTo(earth);
 
   
-// rotation animation
-var before = null;
-      requestAnimationFrame(function animate(now) {
-          var c = earth.getPosition();
-          var elapsed = before? now - before: 0;
-          before = now;
-          earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
-          requestAnimationFrame(animate);
-      });
+  // rotation animation
+  var before = null;
+        requestAnimationFrame(function animate(now) {
+            var c = earth.getPosition();
+            var elapsed = before? now - before: 0;
+            before = now;
+            earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
+            requestAnimationFrame(animate);
+        });
 
-// ISS ajax api call -- collect lat/lon cleanly
-var queryISSURL = "http://api.open-notify.org/iss-now.json";
-  $.ajax({
-      url: queryISSURL,
-      method: "GET"
-  }).then(function(response) {
-      console.log(response);
-      var lat = response.iss_position.latitude;
-      var lon = response.iss_position.longitude;
-      console.log(lat);
-      console.log(lon);
-      // marker basic. pass in ISS value here? edit: hell yesssssss
-      var marker = WE.marker([lat, lon]).addTo(earth)
-      marker.bindPopup('<b>Hello World</b>'); 
-  })
+  // ISS ajax api call -- collect lat/lon cleanly
+  var queryISSURL = "http://api.open-notify.org/iss-now.json";
+    $.ajax({
+        url: queryISSURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var lat = response.iss_position.latitude;
+        var lon = response.iss_position.longitude;
+        console.log("ISS Latitude: "+ lat);
+        console.log("ISS Longitude: "+ lon);
+        // marker basic. pass in ISS value here? edit: hell yesssssss
+        var marker = WE.marker([lat, lon]).addTo(earth)
+        marker.bindPopup('<b>Hello World</b>'); 
+    })
 // closing tag for intialize function
 }
 
